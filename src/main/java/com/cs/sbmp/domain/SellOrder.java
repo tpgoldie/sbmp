768cf3f0ea2ec4848320c987f60a88fb8bfe9c69
Tony Golding <tpg@blueyonder.co.uk>
2017-02-12 21:05:19 +0000

    Create a buy order

--- src/main/java/com/cs/sbmp/domain/SellOrder.java
+++ src/main/java/com/cs/sbmp/domain/SellOrder.java
@@ -3,12 +3,7 @@ package com.cs.sbmp.domain;
 import static com.cs.sbmp.domain.OrderType.SELL;
 
 public class SellOrder extends Order {
-    public SellOrder(String userId, Quantity quantity, Money price) {
-        super(userId, quantity, price);
-    }
-
-    @Override
-    public OrderType getOrderType() {
-        return SELL;
+    SellOrder(String userId, Quantity quantity, Money price) {
+        super(userId, SELL, quantity, price);
     }
 }
